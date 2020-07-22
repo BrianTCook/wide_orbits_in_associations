74cbddccdc4784862b7cfac9eb95801fe2e80334
Brian Cook <btcook@umich.edu>
2020-07-22 11:30:05 -0400

    put class definitions in simulation_script.py directly

--- src/simulation_script.py
+++ src/simulation_script.py
@@ -15,7 +15,80 @@ import matplotlib.pyplot as plt
 from amuse.lab import *
 from amuse.couple import bridge
 from initial_conditions import initial_conditions
-#from amuse_classes import BaseCode, Gravity, Hydro
+
+###BOOKLISTSTART1###
+class BaseCode:
+    def __init__(self, code, particles, eps=0|units.RSun):
+
+        self.local_particles = particles
+        m = self.local_particles.mass.sum()
+        l = self.local_particles.position.length()
+        self.converter = nbody_system.nbody_to_si(m, l)
+        self.code = code(self.converter)
+        self.code.parameters.epsilon_squared = eps**2
+
+    def evolve_model(self, time):
+        self.code.evolve_model(time)
+    def copy_to_framework(self):
+        self.channel_to_framework.copy()
+    def get_gravity_at_point(self, r, x, y, z):
+        return self.code.get_gravity_at_point(r, x, y, z)
+    def get_potential_at_point(self, r, x, y, z):
+        return self.code.get_potential_at_point(r, x, y, z)
+    def get_timestep(self):
+        return self.code.parameters.timestep
+    @property
+    def model_time(self):            
+        return self.code.model_time
+    @property
+    def particles(self):
+        return self.code.particles
+    @property
+    def total_energy(self):
+        return self.code.kinetic_energy + self.code.potential_energy
+    @property
+    def stop(self):
+        return self.code.stop
+###BOOKLISTSTOP1###
+
+###BOOKLISTSTART2###
+class Gravity(BaseCode):
+    def __init__(self, code, particles, eps=0|units.RSun):
+        BaseCode.__init__(self, code, particles, eps)
+        self.code.particles.add_particles(self.local_particles)
+        self.channel_to_framework \
+            = self.code.particles.new_channel_to(self.local_particles)
+        self.channel_from_framework \
+            = self.local_particles.new_channel_to(self.code.particles)
+        self.initial_total_energy = self.total_energy
+###BOOKLISTSTOP2###
+
+###BOOKLISTSTART3###
+class Hydro(BaseCode):
+    def __init__(self, code, particles, eps=0|units.RSun,
+                 dt=None, Rbound=None):
+        BaseCode.__init__(self, code, particles, eps)
+        self.channel_to_framework \
+            = self.code.gas_particles.new_channel_to(self.local_particles)
+        self.channel_from_framework \
+            = self.local_particles.new_channel_to(self.code.gas_particles)
+        self.code.gas_particles.add_particles(particles)
+        m = self.local_particles.mass.sum()
+        l = self.code.gas_particles.position.length()
+        if Rbound is None:
+            Rbound = 10*l
+        self.code.parameters.periodic_box_size = Rbound
+        if dt is None:
+            dt = 0.01*numpy.sqrt(l**3/(constants.G*m))
+        self.code.parameters.timestep = dt/8.
+        self.initial_total_energy = self.total_energy
+    @property
+    def total_energy(self):
+        return self.code.kinetic_energy \
+            + self.code.potential_energy \
+            + self.code.thermal_energy
+###BOOKLISTSTOP3###
+
 
 def simulation(nGas, nStars, diskMass, rMin, rMax, Q, diskmassfrac, tEnd, dt):
 
