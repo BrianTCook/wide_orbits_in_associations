d061c48cf306dfd166bf65d9aa3ffbd7ea0b5175
Brian <btcook@umich.edu>
2020-08-25 17:44:59 +0200

    spyder

--- src/EFF_with_clumps.py
+++ src/EFF_with_clumps.py
@@ -42,7 +42,7 @@ def xyz_coords(Nstars, Nclumps, a, gamma):
 	clump_populations = [ math.ceil(5.*np.random.random()) for i in range(Nclumps) ]
 	Nstars_in_clumps = np.sum(clump_populations)
 
-	Nbins = 25
+	Nbins = 10
 	radial_bins = np.linspace(0., 1.5*a, Nbins+1) #edges of bins in parsecs
 
 	eps_x, eps_y, eps_z = 0.1, 0.1, 0.1 #parsecs, for perturbation purposes
@@ -57,81 +57,72 @@ def xyz_coords(Nstars, Nclumps, a, gamma):
 
 	bin_populations =  [ 0 for i in range(Nbins) ]
 
-	while len(xvals) < (Nstars - Nstars_in_clumps + Nclumps):
+	Nsystems = Nstars - Nstars_in_clumps + Nclumps
 
-		whole_flag = 0
-		clump_flag = 0
+	clump_flag = 0
 
-		while whole_flag == 0:
+	while len(xvals) < Nsystems:
 
-			rval_proposed = 1.5*a*np.random.rand()
-			yval_a, yval_b = EFF(rval_proposed, a, gamma), np.random.rand()
+		rval_proposed = 1.5 * a * np.random.rand() #pc
 
-			#rejection sampling
-			if yval_b < yval_a:
+		j = 0
 
-				j = 0
+		#finds the appropriate bin
+		if radial_bins[j] < rval_proposed and radial_bins[j+1] >= rval_proposed:
 
-				#finds the appropriate bin
-				if radial_bins[j] < rval_proposed and radial_bins[j+1] >= rval_proposed:
+			bin_middle = 0.5*(radial_bins[j]+radial_bins[j+1])
 
-					bin_middle = 0.5*(radial_bins[j]+radial_bins[j+1])
+			#fills bin with a clump or just a single star
+			if clump_flag < Nclumps:
 
-					print('bin middle: %.02f pc'%(bin_middle))
-					#fills bin with a clump or just a single star
-					if clump_flag < Nclumps:
+				new_members = clump_populations[clump_flag]
 
-						new_members = clump_populations[clump_flag]
-						clump_flag += 1
+			else:
 
-					else:
+				new_members = 1
 
-						new_members = 1
+			new_bin_population = bin_populations[j] + new_members
 
-					new_bin_population = bin_populations[j] + new_members
+			if new_bin_population < allowances[j]:
 
-					print('new_bin_population: %i'%(new_bin_population))
-					print('allowance: %i'%(allowances[j]))
+				if clump_flag < Nclumps:
+					clump_flag += 1
 
-					if new_bin_population < allowances[j]:
+				rvals_new = [rval_proposed for k in range(new_members)]
+				phivals_new = [2*np.pi*np.random.random() for k in range(new_members)]
+				thetavals_new = [np.arccos((2.*np.random.random()-1))for k in range(new_members)]
 
-						rvals_new = [rval_proposed for k in range(new_members)]
-						phivals_new = [2*np.pi*np.random.random() for k in range(new_members)]
-						thetavals_new = [np.arccos((2.*np.random.random()-1))for k in range(new_members)]
+				xvals_new = [ rvals_new[k] * np.cos(phivals_new[k]) * np.sin(thetavals_new[k]) for k in range(new_members) ]
+				yvals_new = [ rvals_new[k] * np.sin(phivals_new[k]) * np.sin(thetavals_new[k]) for k in range(new_members) ]
+				zvals_new = [ rvals_new[k] * np.cos(thetavals_new[k]) for k in range(new_members) ]
 
-						xvals_new = [ rvals_new[k] * np.cos(phivals_new[k]) * np.sin(thetavals_new[k]) for k in range(new_members) ]
-						yvals_new = [ rvals_new[k] * np.sin(phivals_new[k]) * np.sin(thetavals_new[k]) for k in range(new_members) ]
-						zvals_new = [ rvals_new[k] * np.cos(thetavals_new[k]) for k in range(new_members) ]
+				#randomly perturb cluster members
+				if new_members > 1:
+				    
+					xvals_new = [ x + eps_x * np.random.rand() for x in xvals_new ]
+					yvals_new = [ y + eps_y * np.random.rand() for y in yvals_new ]
+					zvals_new = [ z + eps_z * np.random.rand() for z in zvals_new ]
 
-						#randomly perturb cluster members
-						if new_members > 1:
-						    
-							xvals_new = [ x + eps_x * np.random.rand() for x in xvals_new ]
-							yvals_new = [ y + eps_y * np.random.rand() for y in yvals_new ]
-							zvals_new = [ z + eps_z * np.random.rand() for z in zvals_new ]
+				xvals.append(xvals_new)
+				yvals.append(yvals_new)
+				zvals.append(zvals_new)
 
-						xvals.append(xvals_new)
-						yvals.append(yvals_new)
-						zvals.append(zvals_new)
+				print(xvals)
 
-						xvals = [ j for i in xvals for j in i ]
-						yvals = [ j for i in yvals for j in i ]
-						zvals = [ j for i in zvals for j in i ]
+				bin_populations[j] += new_members
+		else:
 
-						bin_populations[j] += new_members
+			j += 1
 
+		if j >= Nbins-1:
 
-					whole_flag = 1
-
-				else:
-
-					j += 1
-
-				if j >= Nbins-1:
-
-					print('we should not be here')
-					whole_flag = 1 #should not get here
+			print('we should not be here')
+			return
     
+	xvals = [ j for i in xvals for j in i ]
+	yvals = [ j for i in yvals for j in i ]
+	zvals = [ j for i in zvals for j in i ]
+
 	return xvals, yvals, zvals
     
     
