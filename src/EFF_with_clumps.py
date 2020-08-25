643c2fb9952167ee3f9ef9f229b73751e732ba42
Brian <btcook@umich.edu>
2020-08-25 17:24:10 +0200

    look at it with spyder

--- src/EFF_with_clumps.py
+++ src/EFF_with_clumps.py
@@ -33,19 +33,28 @@ def enclosed_number(Nstars, r, a, gamma):
     
         #normalizing factor has to be 
     
-        return Nstars * ( 1 - (1 - (r/a)**2)**(-gamma/2.+1) )
+        return Nstars * ( 1 - (1 + (r/a)**2)**(-gamma/2.+1) )
     
 def xyz_coords(Nstars, Nclumps, a, gamma):
 
 	xvals, yvals, zvals = [], [], []
 
-	clump_populations = [ math.ceil(40.*np.random.random()) + 10 for i in range(Nclumps) ]
-    Nstars_in_clumps = np.sum(clump_populations)
-
-	print('clump populations: ', clump_populations)
+	clump_populations = [ math.ceil(5.*np.random.random()) for i in range(Nclumps) ]
+	Nstars_in_clumps = np.sum(clump_populations)
 
 	Nbins = 25
 	radial_bins = np.linspace(0., 1.5*a, Nbins+1) #edges of bins in parsecs
+
+	eps_x, eps_y, eps_z = 0.1, 0.1, 0.1 #parsecs, for perturbation purposes
+
+	cdf = [ math.ceil(enclosed_number(Nstars, 0.5*(radial_bins[i] + radial_bins[i+1]), a, gamma)) for i in range(Nbins) ]
+
+	allowances = [ 0 for i in range(Nbins) ]
+
+	for i in range(Nbins):
+
+		allowances[i] = int(cdf[i] - np.sum(allowances[:i]))	
+
 	bin_populations =  [ 0 for i in range(Nbins) ]
 
 	while len(xvals) < (Nstars - Nstars_in_clumps + Nclumps):
@@ -53,11 +62,7 @@ def xyz_coords(Nstars, Nclumps, a, gamma):
 		whole_flag = 0
 		clump_flag = 0
 
-		print('number of assignments: %i'%(len(xvals)))
-
 		while whole_flag == 0:
-            
-            print('clump_flag: %i'%(clump_flag))
 
 			rval_proposed = 1.5*a*np.random.rand()
 			yval_a, yval_b = EFF(rval_proposed, a, gamma), np.random.rand()
@@ -72,10 +77,12 @@ def xyz_coords(Nstars, Nclumps, a, gamma):
 
 					bin_middle = 0.5*(radial_bins[j]+radial_bins[j+1])
 
+					print('bin middle: %.02f pc'%(bin_middle))
 					#fills bin with a clump or just a single star
 					if clump_flag < Nclumps:
 
 						new_members = clump_populations[clump_flag]
+						clump_flag += 1
 
 					else:
 
@@ -83,31 +90,38 @@ def xyz_coords(Nstars, Nclumps, a, gamma):
 
 					new_bin_population = bin_populations[j] + new_members
 
-					if new_bin_population < enclosed_number(Nstars, bin_middle, a, gamma):
+					print('new_bin_population: %i'%(new_bin_population))
+					print('allowance: %i'%(allowances[j]))
+
+					if new_bin_population < allowances[j]:
 
 						rvals_new = [rval_proposed for k in range(new_members)]
 						phivals_new = [2*np.pi*np.random.random() for k in range(new_members)]
 						thetavals_new = [np.arccos((2.*np.random.random()-1))for k in range(new_members)]
 
-                        	xvals_new = [ rvals[i] * np.cos(phivals[k]) * np.sin(thetavals[k]) for k in range(new_members) ]
-                        	yvals_new = [ rvals[i] * np.sin(phivals[k]) * np.sin(thetavals[k]) for k in range(new_members) ]
-                        	zvals_new = [ rvals[i] * np.cos(thetavals[k]) for k in range(new_members) ]
+						xvals_new = [ rvals_new[k] * np.cos(phivals_new[k]) * np.sin(thetavals_new[k]) for k in range(new_members) ]
+						yvals_new = [ rvals_new[k] * np.sin(phivals_new[k]) * np.sin(thetavals_new[k]) for k in range(new_members) ]
+						zvals_new = [ rvals_new[k] * np.cos(thetavals_new[k]) for k in range(new_members) ]
 
-                        #randomly perturb cluster members
-                        if new_members > 1:
-                            
-                            xvals_new = [ x + eps_x * np.random.rand() for x in xvals_new ]
-                            yvals_new = [ y + eps_y * np.random.rand() for y in yvals_new ]
-                            zvals_new = [ z + eps_z * np.random.rand() for z in zvals_new ]
-                            
-                        xvals.append(xvals_new)
-                        yvals.append(yvals_new)
-                        zvals.append(zvals_new)
+						#randomly perturb cluster members
+						if new_members > 1:
+						    
+							xvals_new = [ x + eps_x * np.random.rand() for x in xvals_new ]
+							yvals_new = [ y + eps_y * np.random.rand() for y in yvals_new ]
+							zvals_new = [ z + eps_z * np.random.rand() for z in zvals_new ]
 
-						bin_populations[j] += 1
-						clump_flag += 1
+						xvals.append(xvals_new)
+						yvals.append(yvals_new)
+						zvals.append(zvals_new)
 
-						whole_flag = 1
+						xvals = [ j for i in xvals for j in i ]
+						yvals = [ j for i in yvals for j in i ]
+						zvals = [ j for i in zvals for j in i ]
+
+						bin_populations[j] += new_members
+
+
+					whole_flag = 1
 
 				else:
 
@@ -117,11 +131,6 @@ def xyz_coords(Nstars, Nclumps, a, gamma):
 
 					print('we should not be here')
 					whole_flag = 1 #should not get here
-
-    #concatenate lists
-	xvals = [j for i in xvals for j in i]
-	yvals = [j for i in yvals for j in i]
-	zvals = [j for i in zvals for j in i]
     
 	return xvals, yvals, zvals
     
