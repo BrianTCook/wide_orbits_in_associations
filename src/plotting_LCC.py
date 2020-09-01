cfaf0a4785126a088278ef12b0425bea44fa0c73
Brian Cook <btcook@umich.edu>
2020-09-01 12:21:02 -0400

    have figures for meeting

--- src/plotting_LCC.py
+++ src/plotting_LCC.py
@@ -15,119 +15,131 @@ plt.rc('font', family='serif')
 
 wide_orbits_direc = '/Users/BrianTCook/Desktop/wide_orbits_in_associations/'
 
-phasespace_files = glob.glob(wide_orbits_direc + 'data/phasespace_*.ascii')
-times = np.loadtxt(wide_orbits_direc + 'data/snapshot_times.txt')
+time_arrows = [ 'forward', 'backward' ]
 
-times = [ 15. + t for t in times ]
-
-galaxy_mass_list = np.loadtxt(wide_orbits_direc + 'data/snapshot_galaxy_masses.txt')
-masses = np.loadtxt(wide_orbits_direc + 'data/LCC_masses.txt')
-
-total_mass = np.sum(masses)
-
-hl_radius = 15.48
-angles = np.linspace(0., 2*np.pi, 100)
-xcirc = [ hl_radius * np.cos(angle) for angle in angles ]
-ycirc = [ hl_radius * np.sin(angle) for angle in angles ]
-
-for j, file in enumerate(phasespace_files):
-
-    print('j: %i'%(j))
+for arrow in time_arrows:
     
-    data = np.loadtxt(file)
-
-    Nobjects = len(data[:,0])
-    xvals_stars_and_planets = data[:,0]
-    yvals_stars_and_planets = data[:,1]
-    zvals_stars_and_planets = data[:,2]
+    phasespace_files = glob.glob(wide_orbits_direc + 'data/%s_in_time/phasespace_%s_*.ascii'%(arrow, arrow))
+    times = np.loadtxt(wide_orbits_direc + 'data/%s_in_time/snapshot_times_%s.txt'%(arrow, arrow))
     
-    x_med = np.median(xvals_stars_and_planets)
-    y_med = np.median(yvals_stars_and_planets)
-    z_med = np.median(zvals_stars_and_planets)
+    galaxy_mass_list = np.loadtxt(wide_orbits_direc + 'data/%s_in_time/snapshot_galaxy_masses_%s.txt'%(arrow, arrow))
+    masses = np.loadtxt(wide_orbits_direc + 'data/LCC_masses.txt')
     
-    R_GC = np.sqrt(x_med**2. + y_med**2. + z_med**2.)
+    total_mass = np.sum(masses)
+    
+    hl_radius = 15.48
+    angles = np.linspace(0., 2*np.pi, 100)
+    xcirc = [ hl_radius * np.cos(angle) for angle in angles ]
+    ycirc = [ hl_radius * np.sin(angle) for angle in angles ]
+    
+    for j, file in enumerate(phasespace_files):
+    
+        print('j: %i'%(j))
         
-    xvals_LCC_frame = [ x-x_med for x in xvals_stars_and_planets ]
-    yvals_LCC_frame = [ y-y_med for y in yvals_stars_and_planets ]
-    zvals_LCC_frame = [ z-z_med for z in zvals_stars_and_planets ]
-
-
-    if j == 0:
-
-        rvals_LCC_frame_naught = [ np.sqrt(xvals_LCC_frame[i]**2. + yvals_LCC_frame[i]**2.) for i in range(Nobjects) ]
+        data = np.loadtxt(file)
+    
+        Nobjects = len(data[:,0])
+        xvals_stars_and_planets = data[:,0]
+        yvals_stars_and_planets = data[:,1]
+        zvals_stars_and_planets = data[:,2]
         
-        hist_naught, edges_naught = np.histogram(rvals_LCC_frame_naught, bins='auto')
-        rvals_plot_naught = [ 0.5*(edges_naught[i]+edges_naught[i+1]) for i in range(len(edges_naught)-1) ]
+        x_med = np.median(xvals_stars_and_planets)
+        y_med = np.median(yvals_stars_and_planets)
+        z_med = np.median(zvals_stars_and_planets)
         
-        dr_naught = rvals_plot_naught[1] - rvals_plot_naught[0]
-        annuli_areas = [ 2 * np.pi * r * dr_naught for r in rvals_plot_naught ]
+        R_GC = np.sqrt(x_med**2. + y_med**2. + z_med**2.)
+            
+        xvals_LCC_frame = [ x-x_med for x in xvals_stars_and_planets ]
+        yvals_LCC_frame = [ y-y_med for y in yvals_stars_and_planets ]
+        zvals_LCC_frame = [ z-z_med for z in zvals_stars_and_planets ]
+    
+    
+        if j == 0:
+    
+            rvals_LCC_frame_naught = [ np.sqrt(xvals_LCC_frame[i]**2. + yvals_LCC_frame[i]**2.) for i in range(Nobjects) ]
+            
+            hist_naught, edges_naught = np.histogram(rvals_LCC_frame_naught, bins='auto')
+            rvals_plot_naught = [ 0.5*(edges_naught[i]+edges_naught[i+1]) for i in range(len(edges_naught)-1) ]
+            
+            dr_naught = rvals_plot_naught[1] - rvals_plot_naught[0]
+            annuli_areas = [ 2 * np.pi * r * dr_naught for r in rvals_plot_naught ]
+            
+            hist_naught = [ hist_naught[i]/annuli_areas[i] for i in range(len(annuli_areas)) ]
+            
+        if j > 0:
+            
+            rvals_LCC_frame = [ np.sqrt(xvals_LCC_frame[i]**2. + yvals_LCC_frame[i]**2.) for i in range(Nobjects) ]
+            
+            hist, edges = np.histogram(rvals_LCC_frame, bins='auto')
+            rvals_plot = [ 0.5*(edges[i]+edges[i+1]) for i in range(len(edges)-1) ]
+            
+            dr = rvals_plot[1] - rvals_plot[0]
+            annuli_areas = [ 2 * np.pi * r * dr for r in rvals_plot ]
+            
+            hist = [ hist[i]/annuli_areas[i] for i in range(len(annuli_areas)) ]
+            
+        if arrow == 'forward':
+            
+            time_show = 16. + times[j]
+            
+        if arrow == 'backward':
         
-        hist_naught = [ hist_naught[i]/annuli_areas[i] for i in range(len(annuli_areas)) ]
+            time_show = 16. - times[j]
+            
+        fig, axs = plt.subplots(ncols=1, figsize=(6,6))
         
-    if j > 0:
+        ax1 = axs
+        ax1.set_aspect('equal')
         
-        rvals_LCC_frame = [ np.sqrt(xvals_LCC_frame[i]**2. + yvals_LCC_frame[i]**2.) for i in range(Nobjects) ]
+        ax1.plot(xvals_LCC_frame, yvals_LCC_frame, marker=',', c='k', lw=0, linestyle='')
+        ax1.plot(xcirc, ycirc, c='r', lw=.5, label=r'$r_{\rm half-light, 0}$')
         
-        hist, edges = np.histogram(rvals_LCC_frame, bins='auto')
-        rvals_plot = [ 0.5*(edges[i]+edges[i+1]) for i in range(len(edges)-1) ]
+        Jacobi_radius = R_GC * (total_mass/galaxy_mass_list[j])**(1/3.)
         
-        dr = rvals_plot[1] - rvals_plot[0]
-        annuli_areas = [ 2 * np.pi * r * dr for r in rvals_plot ]
+        xJ = [ Jacobi_radius * np.cos(angle) for angle in angles ]
+        yJ = [ Jacobi_radius * np.sin(angle) for angle in angles ]
         
-        hist = [ hist[i]/annuli_areas[i] for i in range(len(annuli_areas)) ]
+        ax1.plot(xJ, yJ, c='g', lw=.5, label=r'$r_{\rm tidal}$')
         
-    fig, axs = plt.subplots(ncols=1, figsize=(6,6))
-    
-    ax1 = axs
-    ax1.set_aspect('equal')
-    
-    ax1.plot(xvals_LCC_frame, yvals_LCC_frame, marker=',', c='k', lw=0, linestyle='')
-    ax1.plot(xcirc, ycirc, c='r', lw=.5, label=r'$r_{\rm half-light, 0}$')
-    
-    Jacobi_radius = R_GC * (total_mass/galaxy_mass_list[j])**(1/3.)
-    
-    xJ = [ Jacobi_radius * np.cos(angle) for angle in angles ]
-    yJ = [ Jacobi_radius * np.sin(angle) for angle in angles ]
-    
-    ax1.plot(xJ, yJ, c='g', lw=.5, label=r'$r_{\rm tidal}$')
-    
-    ax1.legend(loc='lower right', fontsize=12)
-    
-    ax1.set_xlim(-100., 100.)
-    ax1.set_ylim(-100., 100.)
+        ax1.legend(loc='lower right', fontsize=12)
         
-    ax1.set_title('LCC Model', fontsize=16)
-    ax1.annotate(r'$t_{\rm sim}\sim %.02f$ Myr'%(times[j]), xy=(0.7, 0.85), xycoords='axes fraction', fontsize=12)
-    ax1.annotate(r'$M_{\rm LCC} = %.01f \, M_{\odot}$'%(total_mass), xy=(0.7, 0.8), xycoords='axes fraction', fontsize=12)
-    ax1.set_xlabel(r'$(x-\tilde{x})_{\rm LCC}$ (pc)', fontsize=14)
-    ax1.set_ylabel(r'$(y-\tilde{y})_{\rm LCC}$ (pc)', fontsize=14)
-    fig.tight_layout()
-    plt.savefig('LCC_scatter_%s.pdf'%(str(j).rjust(6, '0')))
-    plt.close()
-    
-    fig, axs = plt.subplots(ncols=1, figsize=(6,6))
-    
-    ax2 = axs
-    ax2.set_aspect('auto')
-    ax2.set_yscale('log')
-    ax2.set_xlim(0., 150.)
-    ax2.set_ylim(1e-3, 5e0)
-    ax2.plot(rvals_plot_naught, hist_naught, label=r'$n(r, t\sim 15.0 \, {\rm Myr})$', linewidth=0.5)
-    
-    if j > 0:
+        ax1.set_xlim(-100., 100.)
+        ax1.set_ylim(-100., 100.)
         
-        ax2.plot(rvals_plot, hist, label=r'$n(r, t\sim %.02f \, {\rm Myr})$'%(times[j]), linewidth=0.5)
-    
-    ax2.axvline(x=hl_radius, label=r'$r_{\rm half-light}(t\sim 15.0 \, {\rm Myr})$', c='r', linewidth=0.5)
-    ax2.axvline(x=Jacobi_radius, label=r'$r_{\rm tidal}(t\sim %.02f \, {\rm Myr})$'%(times[j]), c='g', linewidth=0.5)
-    ax2.legend(loc='center right', fontsize=12)
-    ax2.annotate(r'$n_{\rm today}(r) \propto \left(1 + \left(\frac{r}{a}\right)^{2}\right)^{-\gamma/2}$', xy=(0.5, 0.85), xycoords='axes fraction', fontsize=12)
-    ax2.annotate(r'$a = 50.1$ pc, $\gamma = 15.2$', xy=(0.5, 0.8), xycoords='axes fraction', fontsize=12)
-    
-    ax2.set_xlabel(r'$r_{\rm LCC}$ (pc)', fontsize=14)
-    ax2.set_ylabel(r'$n(r) \hspace{2mm} [N_{\star}/{\rm pc}^{2}]$', fontsize=14)
-    
-    ax2.set_title('LCC Number Density Profile', fontsize=16)
-    fig.tight_layout()
-    fig.savefig('LCC_number_density_%s.pdf'%(str(j).rjust(6, '0')))
-    plt.close()
+        ax1.set_title('LCC Model', fontsize=16)
+        ax1.annotate(r'$t_{\rm sim}\sim %.02f$ Myr'%(time_show), xy=(0.7, 0.85), xycoords='axes fraction', fontsize=12)
+        ax1.annotate(r'$M_{\rm LCC} = %.01f \, M_{\odot}$'%(total_mass), xy=(0.7, 0.8), xycoords='axes fraction', fontsize=12)
+        ax1.set_xlabel(r'$(x-\tilde{x})_{\rm LCC}$ (pc)', fontsize=14)
+        ax1.set_ylabel(r'$(y-\tilde{y})_{\rm LCC}$ (pc)', fontsize=14)
+        fig.tight_layout()
+        plt.savefig('LCC_scatter_%s_%s.pdf'%(str(j).rjust(6, '0'), arrow))
+        plt.close()
+        
+        fig, axs = plt.subplots(ncols=1, figsize=(6,6))
+        
+        ax2 = axs
+        ax2.set_aspect('auto')
+        ax2.set_yscale('log')
+        ax2.set_xlim(0., 150.)
+        ax2.set_ylim(1e-3, 5e0)
+        ax2.plot(rvals_plot_naught, hist_naught, label=r'$n(r, t\sim 16.0 \, {\rm Myr})$', linewidth=0.5)
+        
+        if j > 0:
+            
+            ax2.plot(rvals_plot, hist, label=r'$n(r, t\sim %.02f \, {\rm Myr})$'%(time_show), linewidth=0.5)
+        
+        ax2.axvline(x=hl_radius, label=r'$r_{\rm half-light}(t\sim 16.0 \, {\rm Myr})$', c='r', linewidth=0.5)
+        ax2.axvline(x=Jacobi_radius, label=r'$r_{\rm tidal}(t\sim %.02f \, {\rm Myr})$'%(time_show), c='g', linewidth=0.5)
+        ax2.legend(loc='center right', fontsize=12)
+        ax2.annotate(r'$n_{\rm today}(r) \propto \left(1 + \left(\frac{r}{a}\right)^{2}\right)^{-\gamma/2}$', xy=(0.5, 0.85), xycoords='axes fraction', fontsize=12)
+        ax2.annotate(r'$a = 50.1$ pc, $\gamma = 15.2$', xy=(0.5, 0.8), xycoords='axes fraction', fontsize=12)
+        
+        ax2.set_xlabel(r'$r_{\rm LCC}$ (pc)', fontsize=14)
+        ax2.set_ylabel(r'$n(r) \hspace{2mm} [N_{\star}/{\rm pc}^{2}]$', fontsize=14)
+        
+        ax2.set_title('LCC Number Density Profile', fontsize=16)
+        fig.tight_layout()
+        fig.savefig('LCC_number_density_%s_%s.pdf'%(str(j).rjust(6, '0'), arrow))
+        plt.close()
+        
+        
