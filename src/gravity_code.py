4d5a707481d27e850cda3b6972ab7f94553a2bbc
Brian <btcook@umich.edu>
2020-09-09 20:41:07 +0200

    need to get stellar temperatures and luminosities in ascii files

--- src/gravity_code.py
+++ src/gravity_code.py
@@ -23,7 +23,6 @@ from galpy.potential import MWPotential2014, to_amuse
 from galpy.util import bovy_conversion
 from galpy.actionAngle import actionAngleStaeckel
 
-from cluster_maker import orbiter
 from EFF_with_clumps import enclosed_mass
 from nemesis import *
 from nemesis_supplement import *
@@ -41,68 +40,68 @@ def Hill_radius(r, Msub, Mparent):
 
 def solver_codes_initial_setup(code_name, galaxy_code):
     
-    '''
-    will need to ask SPZ if he meant for field, orbiter to be separate in non
-    Nemesis gravity solvers?
-    '''
-    
-    filename = '/home/brian/Desktop/wide_orbits_in_associations/data/LCC_stars_ICs.csv'
-    stars = read_set_from_file(filename, "csv")
-    
-    x_med, y_med, z_med = np.median(stars.x), np.median(stars.y), np.median(stars.z)
-    
-    '''
-    need to compute Hill radius for each star, and then use median for converter_sub units
-    '''
-    
-    Mgal = 0. #in solar masses
-    Rgal, zgal = np.sqrt((x_med/1000.)**2. + (y_med/1000.)**2.), (z_med/1000.) #in kpc
-    R_GC = np.sqrt(Rgal**2. + zgal**2.) #in kpc
-    
-    for pot in MWPotential2014:
-    
-    	Mgal += pot.mass(Rgal, zgal) * mass_in_msol(220., 8.)
-    
-    converter_parent = nbody_system.nbody_to_si(Mgal, R_GC)
-    converter_sub = nbody_system.nbody_to_si(np.median(stars.mass.value_in(units.MSun))|units.MSun, 1.|units.parsec) #masses list is in solar mass units
+	'''
+	will need to ask SPZ if he meant for field, orbiter to be separate in non
+	Nemesis gravity solvers?
+	'''
 
-    gravity = bridge.Bridge(use_threading=False)
+	filename = '/home/brian/Desktop/wide_orbits_in_associations/data/LCC_stars_ICs.csv'
+	stars = read_set_from_file(filename, "csv")
 
-    if code_name != 'nemesis':
+	x_med, y_med, z_med = np.median(stars.x.value_in(units.kpc)), np.median(stars.y.value_in(units.kpc)), np.median(stars.z.value_in(units.kpc))
 
-        stellar = SeBa()
-        stellar.particles.add_particles(stars)
+	'''
+	need to compute Hill radius for each star, and then use median for converter_sub units
+	'''
 
-        #bridges each cluster with the bulge, not the other way around though
-        herm = Hermite(converter_parent)
-        herm.particles.add_particles(stars)
-        gravity.add_system(herm, (galaxy_code,))  
-            
-    if code_name == 'nemesis':
-        
-        all_bodies = stars
-        
-        '''
-        need add_subsystem and assign_subsystem in HierarchicalParticles I think
-        '''
-        
-        parts=HierarchicalParticles(all_bodies)
-        
-        dt=smaller_nbody_power_of_two(2.|units.Myr, converter_parent)
-        print('dt_nemesis is %.04f Myr'%(dt.value_in(units.Myr)))
-        
-        nemesis=Nemesis(parent_worker, sub_worker, py_worker)
-        nemesis.timestep=dt
-        nemesis.distfunc=timestep
-        nemesis.threshold=dt
-        nemesis.radius=radius
-        nemesis.commit_parameters()
-        nemesis.particles.add_particles(parts)
-        nemesis.commit_particles()
-        
-        stellar = SeBa()
-        stellar.particles.add_particles(all_bodies)
+	Mgal = 0. #in solar masses
+	Rgal, zgal = np.sqrt(x_med**2. + y_med**2.), z_med #in kpc
+	R_GC = np.sqrt(Rgal**2. + zgal**2.) #in kpc
 
-        gravity.add_system(nemesis, (galaxy_code,))
+	for pot in MWPotential2014:
 
-    return gravity.particles, gravity, stellar
+		Mgal += pot.mass(Rgal, zgal) * bovy_conversion.mass_in_msol(220., 8.)
+
+	converter_parent = nbody_system.nbody_to_si(Mgal|units.MSun, R_GC|units.kpc)
+	converter_sub = nbody_system.nbody_to_si(np.median(stars.mass.value_in(units.MSun))|units.MSun, 1.|units.parsec) #masses list is in solar mass units
+
+	gravity = bridge.Bridge(use_threading=False)
+
+	if code_name != 'nemesis':
+
+		stellar = SeBa()
+		stellar.particles.add_particles(stars)
+
+		#bridges each cluster with the bulge, not the other way around though
+		herm = Hermite(converter_parent)
+		herm.particles.add_particles(stars)
+		gravity.add_system(herm, (galaxy_code,))  
+	    
+	if code_name == 'nemesis':
+
+		all_bodies = stars
+
+		'''
+		need add_subsystem and assign_subsystem in HierarchicalParticles I think
+		'''
+
+		parts=HierarchicalParticles(all_bodies)
+
+		dt=smaller_nbody_power_of_two(2.|units.Myr, converter_parent)
+		print('dt_nemesis is %.04f Myr'%(dt.value_in(units.Myr)))
+
+		nemesis=Nemesis(parent_worker, sub_worker, py_worker)
+		nemesis.timestep=dt
+		nemesis.distfunc=timestep
+		nemesis.threshold=dt
+		nemesis.radius=radius
+		nemesis.commit_parameters()
+		nemesis.particles.add_particles(parts)
+		nemesis.commit_particles()
+
+		stellar = SeBa()
+		stellar.particles.add_particles(all_bodies)
+
+		gravity.add_system(nemesis, (galaxy_code,))
+
+	return gravity.particles, gravity, stellar
--- src/simulation_script.py
+++ src/simulation_script.py
@@ -15,13 +15,13 @@ matplotlib.use('agg')
 import matplotlib.pyplot as plt
 
 from galpy.potential import MWPotential2014, to_amuse
-from galpy.util.bovy_conversion import mass_in_msol
+from galpy.util import bovy_conversion
 
 from amuse.lab import *
 from amuse.couple import bridge
 from amuse.support import io
 
-from gravity_code import gravity_code_initial_setup, Hill_radius
+from gravity_code import solver_codes_initial_setup, Hill_radius
 
 def print_diagnostics(sim_time, t0, simulation_bodies, E_dyn, dE_dyn):
     
@@ -36,19 +36,20 @@ def print_diagnostics(sim_time, t0, simulation_bodies, E_dyn, dE_dyn):
 def simulation(mass_association, Nclumps, time_reversal):
 
 	'''
-    runs gravity + stellar for the LCC model
+	runs gravity + stellar for the LCC model
 	'''
 
-    galaxy_code = to_amuse(MWPotential2014, t=0.0, tgalpy=0.0, reverse=False, ro=None, vo=None)
+	galaxy_code = to_amuse(MWPotential2014, t=0.0, tgalpy=0.0, reverse=False, ro=None, vo=None)
     
-    code_name = 'not nemesis'
-    stars, gravity, stellar = solver_codes_initial_setup(code_name, galaxy_code)
-    
-    channel_from_gravity_to_framework = gravity.particles.new_channel_to(stars)
-    channel_from_framework_to_gravity = stars.new_channel_to(gravity.particles)
-    
-    channel_from_stellar_to_framework = stellar.particles.new_channel_to(stars)
+	code_name = 'not nemesis'
+	stars, gravity, stellar = solver_codes_initial_setup(code_name, galaxy_code)
 
+	channel_from_gravity_to_framework = gravity.particles.new_channel_to(stars)
+	channel_from_framework_to_gravity = stars.new_channel_to(gravity.particles)
+
+	channel_from_stellar_to_framework = stellar.particles.new_channel_to(stars)
+
+	'''
 	cluster_mass = stars.mass.sum()
 	r_halflight = a * np.sqrt(4**(1./gamma) - 1.)
 	r_virial = eta/6. * r_halflight
@@ -61,13 +62,15 @@ def simulation(mass_association, Nclumps, time_reversal):
 	print('-----------------------------------------')
 
 	t_dyn = (t_dyn.value_in(units.Myr))|units.Myr
-    dt_parent = 2.|units.Myr
+	'''
+
+	dt = 2.|units.Myr
 
 	t_backward = 16.|units.Myr
 	t_forward = 48.|units.Myr
 
-    sim_times_unitless = np.arange(0., (t_backward+t_forward).value_in(units.Myr), dt.value_in(units.Myr))
-    sim_times = [ t|units.Myr for t in sim_times_unitless ]
+	sim_times_unitless = np.arange(0., (t_backward+t_forward).value_in(units.Myr), dt.value_in(units.Myr))
+	sim_times = [ t|units.Myr for t in sim_times_unitless ]
 
 	#for 3D numpy array storage
 	Nsavetimes = len(sim_times)
@@ -75,7 +78,6 @@ def simulation(mass_association, Nclumps, time_reversal):
     
 	all_data = np.zeros((Nsavetimes+1, Ntotal, 7))
 	energy_data = np.zeros(Nsavetimes+1)
-	time_data = np.zeros(Nsavetimes+1) 
 
 	#for saving in write_set_to_file
 	filename = 'data_temp.csv'
@@ -95,15 +97,16 @@ def simulation(mass_association, Nclumps, time_reversal):
 
 	t0 = time.time()
 
+	energy_init = gravity.particles.potential_energy() + gravity.particles.kinetic_energy()
+
 	for j, t in enumerate(sim_times):
 
 		energy = gravity.particles.potential_energy() + gravity.particles.kinetic_energy()
 		deltaE = energy/energy_init - 1.
 
-		print_diagnostics(t, t0, stars_and_planets, energy, deltaE)
+		print_diagnostics(t, t0, stars, energy, deltaE)
 
 		energy_data[j_like_index] = deltaE
-		time_data[j_like_index] = t.value_in(units.Myr)
 
 		j_like_index += 1
 		io.write_set_to_file(gravity.particles, filename, 'csv',
@@ -113,7 +116,6 @@ def simulation(mass_association, Nclumps, time_reversal):
 		data_t = pd.read_csv(filename, names=list(attributes))
 		data_t = data_t.drop([0, 1, 2]) #removes labels units, and unit names
 
-		#data_t = data_t.drop(columns=['mass']) #goes from 7D --> 6D
 		data_t = data_t.astype(float) #strings to floats
 
 		all_data[j_like_index, :len(data_t.index), :] = data_t.values
@@ -121,28 +123,29 @@ def simulation(mass_association, Nclumps, time_reversal):
 
 		snapshot_times.append(t.value_in(units.Myr))
 
-		x_med, y_med, z_med = np.median(data_t['x']), np.median(data_t['y']), np.median(data_t['z'])
+		x_med, y_med, z_med = np.median(data_t['x'])/1000., np.median(data_t['y'])/1000., np.median(data_t['z'])/1000.
 
 		Mgal = 0. #in solar masses
-		Rgal, zgal = np.sqrt((x_med/1000.)**2. + (y_med/1000.)**2.), (z_med/1000.) #in kpc
+		Rgal, zgal = np.sqrt(x_med**2. + y_med**2.), z_med #in kpc
 		R_GC = np.sqrt(Rgal**2. + zgal**2.) #in kpc
 
 		for pot in MWPotential2014:
 
-			Mgal += pot.mass(Rgal, zgal) * mass_in_msol(220., 8.)
+			Mgal += pot.mass(Rgal, zgal) * bovy_conversion.mass_in_msol(220., 8.)
 
-		snapshot_galaxy_masses.append(Mgal)
+		snapshot_galaxy_masses.append(Mgal) #in MSun
 
-        stellar.evolve_model(t)
-        channel_from_stellar_to_framework.copy()
+		stellar.evolve_model(t)
+		channel_from_stellar_to_framework.copy()
+
+		channel_from_framework_to_gravity.copy()
+		gravity.evolve_model(t)
+		channel_from_gravity_to_framework.copy()
         
-        channel_from_framework_to_gravity.copy()
-        gravity.evolve_model(t)
-        channel_from_gravity_to_framework.copy()
-        
-	np.savetxt('snapshot_times_%s.txt'%(forward_or_backward), snapshot_times)
+	np.savetxt('snapshot_times_%s.txt'%(forward_or_backward), sim_times_unitless)
 	np.savetxt('snapshot_galaxy_masses_%s.txt'%(forward_or_backward), snapshot_galaxy_masses)
-    
+	np.savetxt('snapshot_deltaEs_%s.txt'%(forward_or_backward), energy_data)
+
 	gravity.stop()
 	stellar.stop()
 
