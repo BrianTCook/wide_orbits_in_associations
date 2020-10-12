ac0680ec3fd5a38f6f212a43fe2cfcbd1280a375
Brian Cook <btcook@umich.edu>
2020-10-12 17:00:57 -0400

    MCMC data available for 2139 star LCC model

--- src/MCMC_EFF_model.py
+++ src/MCMC_EFF_model.py
@@ -21,6 +21,14 @@ def gaussian(x, mu, sig):
     
     return np.exp( -(x - mu)**(2.) / (2. * sig**(2.)) )
 
+def mode_finder(parameters):
+    
+    p_counts, p_edges = np.histogram(parameters, bins=100)
+    p_index = np.where(p_counts == np.amax(p_counts))[0]
+    p_mode = 0.5*(p_edges[p_index]+p_edges[p_index+1])[0]
+    
+    return p_mode
+
 def EFF_model(theta, r):
     
     rho_0, a, gamma = theta
@@ -34,36 +42,39 @@ def enclosed_mass(theta, r):
 
     return mass_enc #no units, although we will need in terms of MSun
 
-def sigma_rho(theta_present, r, delta_r):
+def log_prior_uninformed(thetas):
     
-    rho_0_present, a_present, gamma_present = theta_present
-    
-    return -gamma_present * (r/(a_present)**(2.)) * (1 + (r/a_present)**(2.))**(-gamma_present/2. - 1) * (delta_r / 10.)
-
-def log_prior_uninformed(theta):
-    
+    theta = thetas
     rho_0, a, gamma = theta
+    
     if rho_0 > 0. and a > 0. and gamma > 0.:
         return 0.0
     return -np.inf
 
-def log_prior_informed(theta, theta_previous):
+def log_prior_informed(thetas):
+    
+    theta, theta_previous = thetas
     
     rho_0, a, gamma = theta
     rho_0_previous, a_previous, gamma_previous = theta_previous
     
     if rho_0 > 0. and a > 0. and gamma > 0.:
         
-        rho_0_component = gaussian(rho_0, rho_0_previous, 0.2*rho_0_previous)
-        a_component = gaussian(a, a_previous, 0.2*a_previous)
-        gamma_component = gaussian(gamma, gamma_previous, 0.2*gamma_previous)
+        rho_0_component = gaussian(rho_0, rho_0_previous, 0.25*rho_0_previous)
+        a_component = gaussian(a, a_previous, 0.25*a_previous)
+        gamma_component = gaussian(gamma, gamma_previous, 0.25*gamma_previous)
         
         return rho_0_component * a_component * gamma_component
     
     return -np.inf
 
 
-def log_likelihood(theta, r, mass_enc, mass_enc_err):
+def log_likelihood(thetas, r, mass_enc, mass_enc_err):
+    
+    try:
+        theta, theta_previous = thetas
+    except:
+        theta = thetas
     
     rho_0, a, gamma = theta
     N = len(r)
@@ -76,17 +87,18 @@ def log_likelihood(theta, r, mass_enc, mass_enc_err):
         return -np.inf
     return -0.5*np.sum(summand)
 
-def log_probability_uninformed(theta, r, mass_enc, mass_enc_err):
-    lp = log_prior_uninformed(theta)
+def log_probability(thetas, r, mass_enc, mass_enc_err):
+    
+    try:
+        theta, theta_previous = thetas
+        lp = log_prior_informed(thetas)
+    except:
+        theta = thetas
+        lp = log_prior_uninformed(thetas)
+    
     if not np.isfinite(lp):        
         return -np.inf    
-    return lp + log_likelihood(theta, r, mass_enc, mass_enc_err)
-
-def log_probability_informed(theta, r, mass_enc, mass_enc_err):
-    lp = log_prior_informed(theta)
-    if not np.isfinite(lp):        
-        return -np.inf    
-    return lp + log_likelihood(theta, r, mass_enc, mass_enc_err)
+    return lp + log_likelihood(thetas, r, mass_enc, mass_enc_err)
 
 nll = lambda *args: -log_likelihood(*args)
 times = np.linspace(0., 64., 9)
@@ -101,7 +113,9 @@ for bg_str in background_strs:
     files = glob.glob('/Users/BrianTCook/Desktop/wide_orbits_in_associations/data/forward_%s/PhaseSpace_*_LCC.ascii'%(bg_str))
     
     rho_0_present, a_present, gamma_present = 0.017964432528751385, 50.1, 15.2
-    theta_present = rho_0_present, a_present, gamma_present
+    theta = rho_0_present, a_present, gamma_present
+    
+    thetas = theta
 
     for k, file in enumerate(files):
 
@@ -152,20 +166,21 @@ for bg_str in background_strs:
         
         pos = soln.x + 1e-4 * np.random.randn(16, 3)
         nwalkers, ndim = pos.shape
-        
-        if k == 0:
-        
-            sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability_uninformed, args=(r, mass_enc, mass_enc_err))
-        
-        if k != 0:
-            
-            sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability_informed, args=(r, mass_enc, mass_enc_err))
-        
+ 
+        sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, args=(r, mass_enc, mass_enc_err))
         sampler.run_mcmc(pos, 50000, progress=True)
         
-        flat_samples = sampler.get_chain(discard=10000, thin=4, flat=True) 
+        flat_samples = sampler.get_chain(discard=10000, thin=8, flat=True) 
         
         np.savetxt('MCMC_data_%i_Myr_%s.txt'%(times[k], bg_str), flat_samples)
+        
+        rho_0_mode = mode_finder(flat_samples[:,0])
+        a_mode = mode_finder(flat_samples[:,1])
+        gamma_mode = mode_finder(flat_samples[:,2])
+        
+        theta_previous = rho_0_mode, a_mode, gamma_mode
+        
+        thetas = [ theta, theta_previous ]
         
         labels = [ r'$\rho_{0} \hspace{2mm} [M_{\odot} \, {\rm pc}^{-3}]$', r'$a \hspace{2mm} [{\rm pc}]$', r'$\gamma$' ]
         fig = corner.corner(flat_samples, labels=labels, truths=[rho_0_present, a_present, gamma_present])
